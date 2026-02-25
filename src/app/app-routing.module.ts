b376d5d7d9ae3c69de5db2628fb1c07247c54624
Edgar <edgargarciagonzalez@elpuig.xeill.net>
2026-02-25 19:44:26 +0100

    chats2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,11 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
   },
+
 ];
 
 @NgModule({
