5a68bbf826a54d474217aaa54b04be63ad17fb99
santiicc5 <scardosa@elpuig.xeill.net>
2026-02-25 19:26:33 +0100

    home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class LoginPage {}
+export class LoginPage {
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.router.navigateByUrl('/tabs/tab1');
+  }
+}
