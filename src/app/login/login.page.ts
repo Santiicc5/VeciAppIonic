03842d62fcca32766cc6fe3742549949e1f38786
santiicc5 <scardosa@elpuig.xeill.net>
2026-02-26 20:27:34 +0100

    Login prime

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,19 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
-export class LoginPage {
-  constructor(private router: Router) {}
-
-  goHome() {
-    this.router.navigateByUrl('/tabs/tab1');
-  }
-}
+export class LoginPage {}
